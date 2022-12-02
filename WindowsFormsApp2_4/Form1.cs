7f6f563f49e3feb79b35bd051998f62267cf6d0f
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-02 07:53:15 +0300

    Add files via upload

---
+++ WindowsFormsApp2_4/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                textBox2.Text = "";
+                try
+                {
+                    int a = Convert.ToInt32(textBox1.Text);
+                    if (a > 0)
+                    {
+                        for (int i = a; i >= 0; i--)
+                        {
+                            for (int j = i; j >= 0; j--)
+                            {
+                            textBox2.Text += j; textBox2.Text += "  ";
+                            }
+                        textBox2.Text += Environment.NewLine;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Число должно быть положительным");
+                    }
+                }
+                catch
+                {
+                        MessageBox.Show("Некорректный ввод данных");
+                }
+            }
+        }
+    }
+
+
